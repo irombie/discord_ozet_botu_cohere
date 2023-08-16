f91975af24e1a99c924163e4157a84ebcd3f88dd
Irem Ergun <iremlergun@gmail.com>
2023-08-15 20:47:51 -0700

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+# Diva: Discord Ozet Botu 
+## Cohere API'i kullanarak Discord konusmalari ozetleyen bir bot yazmak
+### Yazilim Ortami Kurulumu:
+
+1. Python ile kod yazacagiz, Python3'un kurulu olduguna emin olun. 
+2. Anaconda veya Miniconda kullanarak gelistirme ortamimizi duzenleyecegiz. Buradan Anaconda indirebilirsiniz: https://docs.anaconda.com/free/anaconda/install/index.html
+3. Bu klasoru kopyaladiktan sonra klasore cd'leyip bu komutu calistirin: `conda env create -f conda_env.yml; conda activate workshop_env`.
+4. Istege bagli olarak Docker da indirip calistirabilirsiniz. Linki burada: https://www.docker.com/products/docker-desktop/ Zamanimiz kalirsa Docker ile ufak bir iki sey yapabiliriz. 
+
+### Discord Ile Ilgili Kurulum:
+Bu adimlarin sonunda kendimize bir Discord sunucusu acip botumuzu oraya eklemis olacagiz. 
+1. Linkteki adimlari takip ederek bir Discord sunucusu ve Discord botu olusturun. https://appmaster.io/tr/blog/discord-botu-nasil-olusturulur-ve-sunucuya-eklenir
+!HENUZ SUNUCUYA EKLEMEYIN! Once botumuzun ayarlarini yapacagiz :)) Bu siteden “Bir bot oluşturun ve onu sunucuya ekleyin, bot jetonunu kopyalayın” basligi altindaki 4. maddeye kadar tamamlayin. 
+2. Botu olusturduktan sonra https://discord.com/developers/applications/ sitesine gidin ve botunuzun ait oldugu uygulamaya tiklayin. 
+3. Sol sagdaki menuden “Bot”a tiklayin ve Message Content Intent’i aktive edip kaydedin.
+4. Ayni menuden “OAuth2”ye tiklayin ve sonra da hemen altindaki “URL Generator”a tiklayin.
+5. Bu sayfada “bot” ve “applications.commands” seceneklerini secin. Bu yeni bir menu acilmasina sebep olacaktir. O menudeki “Text Permissions” sutunu altindaki butun secenekleri isaretleyip secin. 
+6. Bu degisiklikleri yaptiktan sonra bu sayfanin altindaki “Generated URL” kismindaki linki kopyalayip tarayiciniza yapistirin. 1. Asamada yarattiginiz sunucuyu secip “Authorize”a tiklayin. Ve bu hareketle botunuz o sunucuya eklenmis oldu! 
+
+### Cohere API Key
+1. https://cohere.com'a gidin. Hesap olusturup giris yapin. 
+2. https://dashboard.cohere.ai/api-keys linkine gidip API key'inize ulasabilirsiniz.
+
+### env.yml dosyasi olusturma (Dersin basinda bu kismin uzerinden bir kere daha gececegiz)
+
+1. `env.yml` diye bir dosya olusturup `env_example.yml`in icerigini ona kopyalayin. 
+2. `DISCORD_TOKEN` satirina yukarida olusturdugunuz botun tokenini yapistirin. Bu linkte yarattiginiz uygulamaya tiklayarak tokeni bulabilirsiniz: https://discord.com/developers/applications/
+3. `COHERE_API_KEY` satirina Cohere API key'inizi yapistirin.
+4. `GUILD_ID` kismina yukarida yarattiginiz sunucunun ID'sini yapistirin. ID'yi bulmak icin buradaki asamalari takip edebilirsiniz: https://support.discord.com/hc/tr/articles/206346498-Kullan%C4%B1c%C4%B1-Sunucu-Mesaj-ID-sini-Nerden-Bulurum-
---
+++ discord_bot.py
@@ -0,0 +1,128 @@
+import json
+import os
+from types import SimpleNamespace
+
+import aiohttp
+import cohere
+import discord
+import yaml
+from discord import app_commands
+
+
+def yaml_load(path):
+    with open(path, "r") as stream:
+        try:
+            configs = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(
+                f"The config file does not exist, so we get the following error: {exc}"
+            )
+            return None
+    return configs
+
+
+CONFIGS = yaml_load(path=os.path.join(os.getcwd(), "env.yml"))
+
+co = cohere.Client(CONFIGS["COHERE_API_KEY"])
+
+
+async def async_summarize(**options):
+    headers = {
+        "Authorization": "BEARER " + co.api_key,
+        "Content-Type": "application/json",
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            co.api_url + "/summarize", headers=headers, json=options
+        ) as resp:
+            resp = await resp.json()
+            x = json.loads(json.dumps(resp), object_hook=lambda d: SimpleNamespace(**d))
+            return x
+
+
+co.async_summarize = async_summarize  # add async summarize call to cohere client
+
+
+MY_GUILD = discord.Object(id=int(CONFIGS["GUILD_ID"]))
+
+
+class DiscordBot(discord.AutoShardedClient):
+    # init discord bot properly with commands
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents, shard_count=2)
+        self.tree = app_commands.CommandTree(self)
+
+    # init discord bot properly with commands
+    async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
+    async def make_history(self, message, num_historic_msgs=20):
+        history = []
+        messages = [
+            message
+            async for message in message.channel.history(
+                limit=num_historic_msgs, before=message
+            )
+        ]
+        for historic_msg in messages:
+            name = historic_msg.author.name
+            line = ""
+
+            if bool(historic_msg.content):
+                if (
+                    historic_msg.author.name == self.user.name
+                ):  # disregard previous chatbot summaries in the new summary
+                    continue
+                line += historic_msg.clean_content
+            if line:
+                history.insert(0, {name: line})
+        return "\n".join(
+            [
+                f"{list(chat_turn.keys())[0]}: {list(chat_turn.values())[0]}"
+                for chat_turn in history
+            ]
+        )
+
+    def prepare_prompt(self, history, message):
+        return (
+            open("prompts/" + CONFIGS["PROMPT_FILE"])
+            .read()
+            .format(
+                history=history.strip(),
+                message=message,
+            )
+        )
+
+    async def summarize(self, prompt):
+        response = await co.async_summarize(
+            text=prompt,
+            model="summarize-xlarge",
+            length="medium",
+            extractiveness="medium",
+        )
+        return response.summary
+
+    async def on_message(self, message):
+        if (self.user.mentioned_in(message) and message.author != self.user) or (
+            isinstance(message.channel, discord.Thread)
+            and message.channel.owner == self.user
+            and message.author != self.user
+        ):
+            async with message.channel.typing():
+                history = await self.make_history(message, num_historic_msgs=20)
+                prompt = self.prepare_prompt(
+                    history, f"{message.author.name}: {message.clean_content}"
+                )
+                summary = await self.summarize(prompt)
+                await message.channel.send(summary)
+                return
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = DiscordBot(intents=intents)
+
+if __name__ == "__main__":
+    client.run(CONFIGS["DISCORD_TOKEN"])
